5b8edc919e6b76a0a0a0a61343de72a73b357abb
Muhammet Ali Öztürk <alicsharp@hotmail.com>
2019-12-24 02:06:17 +0300

    Language Code Fixed

--- dergiBaba/settings.py
+++ dergiBaba/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'tr-tr'
+LANGUAGE_CODE = 'tr-TR'
 
 TIME_ZONE = 'Europe/Istanbul'
 
