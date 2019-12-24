8ee6b3cd68a3e7d015a538bf7ce6061a50b1cc4e
Muhammet Ali Öztürk <alicsharp@hotmail.com>
2019-12-24 12:10:22 +0300

    debug = false

--- dergiBaba/settings.py
+++ dergiBaba/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!v!!c!n@flj26c5a6xf9a2+z087ak6n8hv&do3u4d#pc(2wwb5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["gsuvoldemath.pythonanywhere.com","127.0.0.1"]
 
